2bcceeb1dda44a7bb4b65864036f14961108cf96
syunsuke <mdrgkb4869smrgkk@icloud.com>
2025-06-28 18:51:28 +0900

    一覧ページエラー解消

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,4 @@
-// // import Image from "next/image";
-// // import styles from "./page.module.css";
 
-// export default function Home() {
-//   return (
-//     <main className="flex min-h-screen items-center justify-center bg-red-500">
-//       {/* <main> */}
-//       {/* <h1 className="text-4xl font-bold text-blue-600"> */}
-//       <h1>
-//         Tailwind CSS 動いてるよー！🎉 すごいね
-//       </h1>
-//       <div className="text-red-500">
-//         ここの色変えたい
-//       </div>
-//     </main>
-//   );
-// }
-
-// src/app/page.tsx
 import { getAllPosts } from '../../lib/posts';
 import Link from 'next/link';
 
