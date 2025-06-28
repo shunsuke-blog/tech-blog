9ee1f57849cff717d63ad164364577db9c8dab38
syunsuke <mdrgkb4869smrgkk@icloud.com>
2025-06-28 19:45:23 +0900

    トップページ編集

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,22 +2,32 @@
 import { getAllPosts } from '@/lib/posts';
 import Link from 'next/link';
 
-export default async function Home() {
-  const posts = await getAllPosts(); // Markdownのフロントマターを読み取る関数
+// export default async function Home() {
+//   const posts = await getAllPosts(); // Markdownのフロントマターを読み取る関数
 
+//   return (
+//     <main className="p-8">
+//       <h1 className="text-2xl font-bold mb-4">📝 技術記事一覧page</h1>
+//       <ul>
+//         {posts.map((post) => (
+//           <li key={post.slug} className="mb-4">
+//             <Link href={`/blogs/${post.slug}`}>
+//               <div className="text-lg font-semibold text-blue-600 hover:underline">{post.title}</div>
+//               <div className="text-sm text-gray-500">{post.date}</div>
+//             </Link>
+//           </li>
+//         ))}
+//       </ul>
+//     </main>
+//   );
+// }
+
+export default function Home() {
   return (
-    <main className="p-8">
-      <h1 className="text-2xl font-bold mb-4">📝 技術記事一覧page</h1>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.slug} className="mb-4">
-            <Link href={`/blogs/${post.slug}`}>
-              <div className="text-lg font-semibold text-blue-600 hover:underline">{post.title}</div>
-              <div className="text-sm text-gray-500">{post.date}</div>
-            </Link>
-          </li>
-        ))}
-      </ul>
+    <main className="flex min-h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Tailwind CSS 動いてるよー！🎉
+      </h1>
     </main>
   );
 }
